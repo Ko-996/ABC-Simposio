6078b452b02a54c9b9f80869ba92444284cf0af8
kevin <kovallel1@miumg.edu.gt>
2025-02-11 20:55:02 -0600

    Inicio del proyecto

---
+++ src/main/java/com/app/webSimposio/AbcSimposioApplication.java
@@ -0,0 +1,13 @@
+package com.app.webSimposio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcSimposioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcSimposioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/webSimposio/AbcSimposioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.webSimposio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcSimposioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
